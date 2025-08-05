5a7fde7f0c8f73498fd311db819861959c2c5f93
nwhye <hannaasiadouskaya@gmail.com>
2025-08-06 00:02:39 +0300

    4th week + 6th week update

--- python/fuel.py
+++ python/fuel.py
@@ -1,27 +1,35 @@
 def main():
-    f = fuel_check("Fraction: ")
-    print(f)
-
-def fuel_check(fraction):
     while True:
         try:
-            x, y = input(fraction).split("/") # работает. делит.
-            x = int(x)
-            y = int(y)
-            if x > y:
-                continue  # go to next cycle of while true loop
-            if x<0 or y<0:
-                continue
-
-            fuel = int(round(x/y * 100))
-            if fuel >= 99:
-                return("F")
-            elif fuel <= 1:
-                return("E")
-            else:
-                return(f"{fuel}%")
+            f = convert("Fraction: ")
+            percent = gauge(f)
+            print(percent)
+            break
 
         except (ValueError, ZeroDivisionError):
             pass
 
-main()
+
+def convert(fraction):
+    x, y = input(fraction).split("/")
+    x = int(x)
+    y = int(y)
+    if x > y:
+        raise ValueError
+    if x<0 or y<0:
+        raise ValueError
+
+    return round(x/y * 100)
+
+
+def gauge(percentage):
+    if percentage >= 99:
+        return "F"
+    elif percentage <= 1:
+        return "E"
+    else:
+        return f"{percentage}%"
+
+
+if __name__ == "__main__":
+    main()
