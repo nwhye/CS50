5a3c91980e6b4579f874ff75bbb1d54c37a1e134
nwhye <hannaasiadouskaya@gmail.com>
2025-09-07 17:05:34 +0300

    notes. Object-Oriented Programming

--- python/lectures/lecture8.py
+++ python/lectures/lecture8.py
@@ -30,6 +30,9 @@ def get_student():
     name = input("Enter your name: ")
     house = input("Enter your house: ")
     return (name, house)  # return a tuple, in which 2 values (can be without brackets)
+# tuple - immutable
+# immutable - can not change the value
+# list and dic are mutable
 
 
 if __name__ == "__main__":
@@ -41,3 +44,45 @@ if __name__ == "__main__":
 def main():
     student = get_student()
     print(f"{student[0]} from {student[1]}")
+
+
+# fixing user input, but we must use list, and not tuple
+
+def main():
+    student = get_student()
+    if student[0] == "Padma":
+        student[1] = "Ravenclaw"
+    print(f"{student[0]} from {student[1]}")
+
+def get_student():
+    name = input("Enter your name: ")
+    house = input("Enter your house: ")
+    return [name, house]
+
+
+# let's optimize so when we have more than 2 value it would be easier to manage
+
+def main():
+    student = get_student()
+    if student["name"] == "Padma":
+        student["house"] = "Ravenclaw"
+    print(f"{student['name']} from {student['student']}")
+
+
+def get_student():
+    student = {}
+    student["name"] = input("Enter your name: ")
+    student["house"] = input("Enter your house: ")
+    return student
+
+# or
+
+def get_student():
+    name = input("Enter your name: ")
+    house = input("Enter your house: ")
+    return {"name": name, "house": house}
+
+
+# --- CLASSES AND OBJECTS
+
+
