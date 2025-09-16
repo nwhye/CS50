83c027c4ac87555735bcedbf69ced4222f1065ff
nwhye <hannaasiadouskaya@gmail.com>
2025-09-16 14:02:02 +0300

    notes. Object-Oriented Programming

--- python/lectures/lecture8.py
+++ python/lectures/lecture8.py
@@ -365,4 +365,38 @@ def main():
 
 # --- INHERITANCE
 
+# for example there Student and Professor, both has names and both has error checking for name
+# how to prevent duplicating such things
 
+class Wizard:
+    def __init__(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self.name = name
+
+    ...
+
+
+class Student(Wizard):  # to mark super-class, from which inherit
+    def __init__(self, name, house):
+        super().__init__(name)  # reference to super class, to parent
+        self.house = house
+
+    ...
+
+
+class Professor(Wizard):
+    def __init__(self, name, subject):
+        super().__init__(name)
+        self.subject = subject
+
+    ...
+
+
+wizard = Wizard("Albus")
+student = Student("Harry", "Gryffindor")
+professor = Professor("Severus", "Defense Against the Dark Arts")
+...
+
+
+# --- Operator Overloading
